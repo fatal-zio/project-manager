b41339f2b511a1b528df636e211fa98443b0452b
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2024-12-27 15:55:29 -0600

    Added logic to display New Project form

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,13 +1,13 @@
 import noProjectImage from "../assets/no-projects.png";
 import Button from "./Button.jsx";
 
-export default function NoProjectSelected() {
+export default function NoProjectSelected({ onStartAddProject }) {
   return (
     <div className="mt-24 text-center w-2/3">
       <img
         src={noProjectImage}
         alt="An empty task list"
-        classname="w-16 h-16 object-contain mx-auto"
+        className="w-16 h-16 object-contain mx-auto"
       />
       <h2 className="text-xl font-bold text-stone-500 my-4">
         No Project Selected
@@ -16,7 +16,7 @@ export default function NoProjectSelected() {
         Select a project or get started with a new one
       </p>
       <p className="mt-8">
-        <Button>Create new project</Button>
+        <Button onClick={onStartAddProject}>Create new project</Button>
       </p>
     </div>
   );
